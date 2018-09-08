e9876ed307bd93d530c3416797a302f51d9de9c4
Georgi Spirov <styxer@mail.bg>
2018-09-08 10:57:48 +0300

    generated users dto

---
+++ model/UsersDTO.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace PulpFiction\models;
+
+class UsersDTO 
+{
+
+    /**
+    * @var int $id 
+    */
+    public $id;
+
+    /**
+    * @var  $username 
+    */
+    public $username;
+
+    /**
+    * @var  $first_name 
+    */
+    public $first_name;
+
+    /**
+    * @var  $last_name 
+    */
+    public $last_name;
+
+    /**
+    * @var  $email 
+    */
+    public $email;
+
+    /**
+    * @var  $created_at 
+    */
+    public $created_at;
+
+    /**
+    * @var  $updated_at 
+    */
+    public $updated_at;
+
+    /**
+    * @var  $gender_id 
+    */
+    public $gender_id;
+
+} 
+?>
